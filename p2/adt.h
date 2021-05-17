543eea3ca69a456307a6a258389de6b1a8d411dc
Ricardo Antunes <me@riscadoa.com>
2021-05-17 17:31:48 +0100

    Implemented doubly linked list

---
+++ p2/adt.h
@@ -0,0 +1,59 @@
+/*
+ * File: 		adt.h
+ * Author: 		Ricardo Antunes
+ * Description: Abstract data type function prototypes are declared here.
+ */
+
+#ifndef TADS_H
+#define TADS_H
+
+struct file;
+struct avl;
+struct list;
+struct link;
+
+/*
+ * Function pointer type passed to traversal functions. If the function returns
+ * a non NULL value, the traversal is ended early.
+ */
+typedef void*(*traverse_fn)(void*, struct file* file);
+
+/*
+ * Function pointer type used by the AVL tree to extract a key from a file
+ * which is then used for comparison.
+ */
+typedef int(*file_hash_fn)(const struct file* file);
+
+/* File ADT function prototypes. */
+
+struct file* file_create(const char* path, struct file* root);
+void file_destroy(struct file* file);
+void file_set(const char* path, char* value, struct file* root);
+
+struct file* file_find(struct file* root, const char* path);
+struct file* file_search(struct file* root, const char* value);
+void file_print(struct file* root);
+void file_list(struct file* root);
+
+const char* file_value(struct file* file);
+const char* file_component(struct file* file);
+int file_time(struct file* file);
+struct file* file_parent(struct file* file);
+
+/* AVL tree ADT function prototypes. */
+
+struct avl* avl_insert(struct avl* avl, struct file* file);
+struct avl* avl_remove(struct avl* avl, struct file* file);
+void avl_destroy(struct avl* avl);
+struct file* avl_find(struct avl* avl, const char* key);
+void* avl_traverse(struct avl* avl, void* ptr, traverse_fn fn);
+
+/* Doubly linked list ADT function prototypes. */
+
+struct list* list_create(void);
+void list_destroy(struct list* list);
+struct link* list_insert(struct list* list, struct file* file);
+void list_remove(struct list* list, struct link* link);
+void* list_traverse(struct list* list, void* ptr, traverse_fn fn);
+
+#endif
---
+++ p2/list.c
@@ -0,0 +1,93 @@
+/*
+ * File: 		list.c
+ * Author: 		Ricardo Antunes
+ * Description: Doubly linked list implementation used by the filesystem.
+ */
+
+#include "adt.h"
+
+#include <malloc.h>
+
+/* Describes a doubly linked list link. */
+struct link {
+	struct file* file;		/* File this link points to */
+	struct link* prev;	/* Previous link, may be NULL */
+	struct link* next;	/* Next link, may be NULL */
+};
+
+/* Describes a doubly linked list. */
+struct list {
+	struct link* first;
+	struct link* last;
+};
+
+/*
+ * Creates a new doubly linked list and returns a pointer to it. If the
+ * allocation fails, NULL is returned.  
+ */
+struct list* list_create(void) {
+	/* calloc initializes list->first and list->last to NULL (0) */
+	return calloc(1, sizeof(struct list));
+}
+
+/* Destroys a doubly linked list, freeing all memory associated with it. */
+void list_destroy(struct list* list) {
+	struct link* link;
+	
+	/* Free all links */
+	while(list->first != NULL) {
+		link = list->first;
+		list->first = link->next;
+		free(link);
+	}
+
+	free(list);
+}
+
+/*
+ * Inserts a new file into a doubly linked list. If the allocation fails, NULL
+ * is returned and the list remains unchanged. Otherwise, a pointer to the new
+ * link which points to the file is returned.
+ */
+struct link* list_insert(struct list* list, struct file* file) {
+	struct link* link = malloc(sizeof(struct link));
+	
+	if (link != NULL) {
+		link->next = NULL;
+		link->prev = list->last;
+		if (list->last != NULL)
+			list->last->next = link;
+		list->last = link;
+	}
+
+	return link;
+}
+
+/* Removes a link from a doubly linked list. */
+void list_remove(struct list* list, struct link* link) {
+	if (link->prev != NULL)
+		link->prev->next = link->next;
+	if (link->next != NULL)
+		link->next->prev = link->prev;
+	if (list->first == link)
+		list->first = link->next;
+	if (list->last == link)
+		list->last = link->prev;
+	free(list);
+}
+
+/*
+ * Traverses a doubly linked list. fn(ptr, file) is called for each file
+ * present in the list. If fn(ptr, file) returns a non-NULL value, the traversal
+ * ends early and that value is returned.
+ */
+void* list_traverse(struct list* list, void* ptr, traverse_fn fn) {
+	struct link* link;
+	void* ret;
+
+	for (link = list->first; link != NULL; link = link->next)
+		if ((ret = fn(ptr, link->file)) != NULL)
+			return ret;
+
+	return NULL;
+}
